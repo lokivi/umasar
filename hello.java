9f60cd895cf5c82de6dd7095490ef3e873cd4532
lokivi <lokeshwarbarani1922002@gmail.com>
2022-09-06 13:45:58 +0530

    twoooooooo

--- hello.java
+++ hello.java
@@ -1,5 +1,6 @@
 public class hello { 
  public static void main(String[] args) { 
- System.out.println("Hello, world fromm GIT!"); 
+ System.out.println("Hello, world from GIT!"); 
+ System.out.println("Hello, world from GIT changed 1 st time!"); 
  }
 }
